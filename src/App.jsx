b7b2dad89b4d549cc7b544d7d12c6bcb8a69032b
ShobhitPatra <shobhit9999999@gmail.com>
2024-03-12 14:43:20 +0530

    todo assignment

--- src/App.jsx
+++ src/App.jsx
@@ -2,33 +2,36 @@ import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import axios from "axios";
 
 function App() {
-  const [render, setRender] = useState(true);
-
+  const [todos, setTodos] = useState([]);
   useEffect(() => {
-    setInterval(() => {
-      setRender((r) => !r);
-    }, 3000);
+    axios
+      .get("https://sum-server.100xdevs.com/todos")
+      .then((response) => setTodos(response.data.todos));
   }, []);
 
-  return <>{render ? <Component /> : <div>empty</div>}</>;
-}
-
-const Component = () => {
-  useEffect(() => {
-    console.log("mounted");
-
-    return () => {
-      console.log("unmounted");
-    };
-  }, []);
-  
   return (
     <>
-      <div>Hello from Component</div>
+      {todos.map((res) => (
+        <Format todo={res} key={res.id} />
+      ))}
     </>
   );
-};
+}
+
+function Format({ todo }) {
+  return (
+    <>
+      <div>Title :{todo.title}</div>
+      <div>Description :{todo.description}</div>
+      <br />
+      <br />
+      <br />
+      <br />
+    </>
+  );
+}
 
 export default App;
