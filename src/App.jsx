6bea3d50bb268d971a7c5970c1a7511e6776efb6
ShobhitPatra <shobhit9999999@gmail.com>
2024-03-12 14:21:17 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [render, setRender] = useState(true);
+
+  useEffect(() => {
+    setInterval(() => {
+      setRender((r) => !r);
+    }, 3000);
+  }, []);
+
+  return <>{render ? <Component /> : <div>empty</div>}</>;
+}
+
+const Component = () => {
+  useEffect(() => {
+    console.log("mounted");
+
+    return () => {
+      console.log("unmounted");
+    };
+  }, []);
+
+  return (
+    <>
+      <div>Hello from Component</div>
+    </>
+  );
+};
+
+export default App;
